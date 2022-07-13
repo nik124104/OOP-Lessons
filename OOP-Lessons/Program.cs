f41f62aec33c5deb04d027bbf9d510995c59ffd9
nik124104 <mnkaidak.1994@gmail.com>
2022-07-13 17:34:28 +0300

    Добавьте файлы проекта.

---
+++ OOP-Lessons/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP_Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Домашнее задание!");
+            Console.WriteLine("Lesson2");
+            Console.Write("Запусть дамашне задание №");
+            string lesson= Console.ReadLine();
+
+            switch (lesson)
+            {
+                case "2":
+                    Console.WriteLine("Выбран Lesson2");
+
+                    break;
+            }
+
+        }
+    }
+}
