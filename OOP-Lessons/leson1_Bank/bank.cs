96e7ba55777906bd0dd7d5c22b53c650601581b5
nik124104 <mnkaidak.1994@gmail.com>
2022-07-15 00:35:56 +0300

    backend

--- OOP-Lessons/leson1_Bank/bank.cs
+++ OOP-Lessons/leson1_Bank/bank.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOP_Lessons.leson1_Bank;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,262 @@ using System.Threading.Tasks;
 
 namespace OOP_Lessons
 {
-    public class bank
+    public class Banks
     {
+        private static int newid = 0; //порядковый номер счета
+        //private static var checks;
+        public static void Bank()
+        {
+            int n = 0;
+
+            double money;
+
+
+            var checks = new List<Check>();
+            var check = new Check();
+            checks.Add(check);
+
+            Console.WriteLine("Меню Банка:\n 1)Создать счет\n 2)Пополнить счет\n 3)Списать средства\n 4)Просмотр баланса:\n 5)Выйти \nВыберете пункт меню:");
+            do
+            {
+                int.TryParse(Console.ReadLine(), out n);
+
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Создание нового счета");
+                        newid++;
+                        // NewCheck(newid); //Пытался вписать код в метод, но что то пошло не так)),
+                        // промучился долго...
+                        // Проблема была в том что не смог вынести  List<Check> на весь класс
+                        {
+                            double money = -1;
+                            int tipe = -1;
+                            do
+                            {
+
+
+                                Console.Write("Введите сумму - ");
+                                // money = Convert.ToDouble(Console.ReadLine());
+                                if (double.TryParse(Console.ReadLine(), out money) & money >= 0)
+                                {
+                                    Log.Logs($"Принято money={money}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Введите коректную сумму больше 0");
+                                    money = -1;
+                                }
+                                Console.Write("Введите тип счета 1)Classic, 2)salary, 3)credit ");
+                                //int tipe = Convert.ToInt32(Console.ReadLine());
+
+                                if (int.TryParse(Console.ReadLine(), out tipe) & tipe > 0 & tipe < 4)
+                                {
+                                    Log.Logs($"Принято Тип счета={tipe}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Введите коректную сумму больше 0");
+                                    money = -1;
+                                }
+
+                            } while (money > 0 & tipe > 0 & tipe < 4);
+
+
+
+                            check = new Check(1000 + newid, money, tipe);
+                            checks.Add(check);
+                        }
+                        break;
+                    case 2:
+
+                        Console.WriteLine("Пополнение счета");
+                        Console.Write("Введите номер счета");
+
+                        {
+                            int idcheck;
+
+                            idcheck = ID();
+                            var id1 = checks.FirstOrDefault(u => u.idcheck == idcheck);
+
+                            if (id1.idcheck == idcheck)///Не забыть посмотреть
+                            {
+                                Console.Write("Введите сумму пополнения + ");
+
+                                if (double.TryParse(Console.ReadLine(), out money) & money > 0)
+                                {
+                                    Log.Logs($"Принято money={money}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Вы вели не коректную сумму пополнения");
+                                    money = 0;
+
+                                }
+
+                                if (money > 0)
+                                {
+                                    id1.balanse += money;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Счет не найден");
+                            }
+
+                            Console.WriteLine("Выход в главное меню банка");
+
+                        }
+                        break;
+                    case 3:
+                        {
+                            int idcheck;
+                            Console.WriteLine("Списание средств");
+                            idcheck = ID();
+                            var id2 = checks.FirstOrDefault(u => u.idcheck == idcheck);
+
+
+                            if (id2.idcheck == idcheck) ///Не забыть посмотреть!!!
+                            {
+                                Console.Write("Введите сумму пополнения - ");
+
+                                money = MOney();
+
+                                if (money > 0)
+                                {
+                                    id2.balanse -= money;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Счет не найден");
+                            }
+
+
+                        }
+                        break;
+                    case 4:
+                        {
+                            int idcheck;
+                            Console.WriteLine("Списание средств");
+                            idcheck = ID();
+                            var id3 = checks.FirstOrDefault(u => u.idcheck == idcheck);
+
+                            if(id3.idcheck == idcheck)
+                            {
+                                Console.WriteLine(id3.balanse);
+                            }
+                            else 
+                            {
+                                Console.WriteLine("Счет не найден");
+                            }
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Выход в главное меню.");
+                        
+                        break;
+                    default:
+                        Console.WriteLine("Введите пункт меню от 1-5");
+                        break;
+
+                }
+            } while (n == 5);
+
+        }
+
+        static int ID()
+        {
+            bool flag = true;
+            int idcheck;
+            do
+            {
+
+                if (int.TryParse(Console.ReadLine(), out idcheck))
+                {
+                    Log.Logs($"Принято номер счета={idcheck}");
+                }
+                else
+                {
+                    Console.WriteLine("Введите коректно номер счета в формате ХХХХ");
+                    flag = false;
+                }
+            } while (flag == true);
+
+            return idcheck;
+        }
+
+
+        static double MOney()
+        {
+
+            double money;
+            if (double.TryParse(Console.ReadLine(), out money) & money > 0)
+            {
+                Log.Logs($"Принято money={money}");
+            }
+            else
+            {
+                Console.WriteLine("Вы вели не коректную сумму списания");
+                money = 0;
+
+            }
+            return money;
+
+        }
+
+        static void NewCheck(int newid)
+        {
+            double money=-1;
+            int tipe = -1;
+            do
+            {
+
+
+                Console.Write("Введите сумму - ");
+                // money = Convert.ToDouble(Console.ReadLine());
+                if (double.TryParse(Console.ReadLine(), out money) & money >= 0)
+                {
+                    Log.Logs($"Принято money={money}");
+                }
+                else
+                {
+                    Console.WriteLine("Введите коректную сумму больше 0");
+                    money = -1;
+                }
+                Console.Write("Введите тип счета 1)Classic, 2)salary, 3)credit ");
+                //int tipe = Convert.ToInt32(Console.ReadLine());
+
+                if (int.TryParse(Console.ReadLine(), out tipe) & tipe > 0 & tipe < 4)
+                {
+                    Log.Logs($"Принято money={money}");
+                }
+                else
+                {
+                    Console.WriteLine("Введите коректную сумму больше 0");
+                    money = -1;
+                }
+
+            } while (money > 0 & tipe > 0 & tipe < 4);
+
+            
+            //var checks = new List<Check>();
+            //var check = new Check(1000 + newid, money, tipe);
+
+
+            ////var check = new Check() { balanse = 100, idcheck=10 };
+            //checks.Add(check);
+
+
+        }
+
+        static void View()
+        {
+            //Console.WriteLine(
+
+        }
         
+
+        }
     }
 }
---
+++ OOP-Lessons/leson1_Bank/check.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lessons.leson1_Bank
+{
+
+    public enum CheckTipe
+    {
+        Classic = 1,
+        salary = 2,
+        credit =3
+
+    }
+
+    public class Check
+    {
+        private int _idcheck;//№Счета
+        private double _balance;//Баланс
+        private CheckTipe _tipe;//Тип счета    
+
+
+        public Check()
+        {
+            _idcheck = 10000;
+
+            _balance = 0;
+
+            _tipe = 0; //Почемуто 0 можно, а int уже нельзя //_tipe = (CheckTipe)tipe;
+        }
+
+        public Check(int idcheck, double balanse, int tipe)
+        {
+            _idcheck = idcheck;
+            _balance = balanse;
+            _tipe = (CheckTipe)tipe;
+
+        }
+
+        public int idcheck
+        {
+            get
+            {
+                return _idcheck;
+            }
+            set
+            {
+                _idcheck = value;
+            }
+        }
+
+        public double balanse
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+
+    }
+}
